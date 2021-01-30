2e9d1b4ebda111eaaa6339afd4731aa5ee836180
Jesse Yang <jesseyang432@gmail.com>
2021-01-29 20:24:59 -0600

    converted to camelCase, Logo-, and className

--- src/components/modules/Logo.js
+++ src/components/modules/Logo.js
@@ -13,31 +13,31 @@ class Logo extends Component {
         <>
             <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="47.36px" y="54.7px"
                 viewBox="0 0 47.36 54.7" style={{enableBackground: "new 0 0 47.36 54.7"}} xmlSpace="preserve">
-                <polygon class="Logo-st0" points="23.6,2.38 1.94,14.88 1.94,39.89 23.6,52.38 45.25,39.88 45.25,14.88 "/>
+                <polygon className="Logo-st0" points="23.6,2.38 1.94,14.88 1.94,39.89 23.6,52.38 45.25,39.88 45.25,14.88 "/>
                 <g>
                     <linearGradient id="Logo-SVGID_1_" gradientUnits="userSpaceOnUse" x1="13.0543" y1="76.3367" x2="27.5845" y2="76.3367" gradientTransform="matrix(1 0 0 -1 0 111.1888)">
                     <stop  offset="0" style={{stopColor:"FF7BAC"}}/>
                     <stop  offset="1" style={{stopColor:"#FF7BAC", stopOpacity:"0"}}/>
                     </linearGradient>
-                    <path class="Logo-st1" d="M19.87,33.89c-0.05-0.19-0.11-0.45-0.15-0.74c-0.02-0.12-0.04-0.31-0.06-0.57c-0.02-0.31-0.02-0.67,0-1.08
+                    <path className="Logo-st1" d="M19.87,33.89c-0.05-0.19-0.11-0.45-0.15-0.74c-0.02-0.12-0.04-0.31-0.06-0.57c-0.02-0.31-0.02-0.67,0-1.08
                     c0-1.76-1.48-3.18-3.3-3.18c-1.82,0-3.3,1.42-3.3,3.18v0.65c1.52,5.35,5.41,9.02,9.6,9.25c1.99,0.11,3.7-0.59,4.93-1.32
                     c-1.8-0.47-3.1-1.2-3.9-1.73C20.84,36.45,19.96,34.24,19.87,33.89z"/>
                     <linearGradient id="Logo-SVGID_2_" gradientUnits="userSpaceOnUse" x1="20.9573" y1="85.2167" x2="32.2558" y2="85.2167" gradientTransform="matrix(1 0 0 -1 0 111.1888)">
                     <stop  offset="0" style={{stopColor:"#FF7BAC"}}/>
                     <stop  offset="1" style={{stopColor:"#FF7BAC", stopOpacity:"0"}}/>
                     </linearGradient>
-                    <path class="Logo-st2" d="M28.95,11.88c-1.82,0-3.3,1.42-3.3,3.18v17.51c0,0.8-0.34,1.52-0.88,2.04c-0.62,0.6-1.35,0.76-1.65,0.81
+                    <path className="Logo-st2" d="M28.95,11.88c-1.82,0-3.3,1.42-3.3,3.18v17.51c0,0.8-0.34,1.52-0.88,2.04c-0.62,0.6-1.35,0.76-1.65,0.81
                     c-0.09,0.01-0.26,0.04-0.47,0.04c-0.74,0-1.33-0.26-1.7-0.47c0.36,0.74,1.16,2.14,2.73,3.33c1.55,1.18,3.1,1.58,3.9,1.73
                     c0.67-0.33,1.48-0.84,2.24-1.63c2.28-2.35,2.42-5.37,2.43-6.29V15.06C32.25,13.31,30.77,11.88,28.95,11.88z"/>
-                    <line class="Logo-st3" x1="40.49" y1="16.13" x2="40.49" y2="38.64"/>
-                    <line class="Logo-st3" x1="24.86" y1="47.83" x2="5.37" y2="36.58"/>
-                    <line class="Logo-st3" x1="5.37" y1="17.92" x2="24.86" y2="6.67"/>
+                    <line className="Logo-st3" x1="40.49" y1="16.13" x2="40.49" y2="38.64"/>
+                    <line className="Logo-st3" x1="24.86" y1="47.83" x2="5.37" y2="36.58"/>
+                    <line className="Logo-st3" x1="5.37" y1="17.92" x2="24.86" y2="6.67"/>
                 </g>
-                <linearGradient id="SVGID_3_" gradientUnits="userSpaceOnUse" x1="22.6542" y1="41.3836" x2="22.6542" y2="41.3836">
+                <linearGradient id="Logo-SVGID_3_" gradientUnits="userSpaceOnUse" x1="22.6542" y1="41.3836" x2="22.6542" y2="41.3836">
                     <stop  offset="0" style={{stopColor:"FF7BAC"}}/>
                     <stop  offset="1" style={{stopColor:"FF7BAC", stopOpacity:"0"}}/>
                 </linearGradient>
-                <path class="Logo-st4" d="M22.65,41.38"/>
+                <path className="Logo-st4" d="M22.65,41.38"/>
             </svg>
 </>
 
