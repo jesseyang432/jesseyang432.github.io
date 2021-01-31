4c475060c46284908aecf4e2d9c513ded5038361
Jesse Yang <jesseyang432@gmail.com>
2021-01-30 22:02:27 -0600

    rest only appears when loading state is set to false

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,20 +6,50 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
+        loading: true,
+    };
+  }
 
-    }
+  componentDidMount = () => {
+    setTimeout(() => {
+        this.setState({loading: false});
+    }, 5000);
   }
 
   render() {
+      console.log(this.state.loading);
     return (
         <>
-            <Nav/>
-            {/* <div className="Home-title">
-                <h1><em>Jesse Yang</em></h1>
-            </div> */}
-            <div className="Home-LogoLoading">
-                <LogoLoading></LogoLoading>
-            </div>
+            ({this.state.loading ? 
+                <LogoLoading></LogoLoading> :
+                <div className="Home-LogoLoading">
+                <Nav/>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                <h1>lots of content</h1>
+                </div>
+            })
         </>
     );
   }
