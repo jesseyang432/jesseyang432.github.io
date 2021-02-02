9f41ec2b274dfc035afd6dc607535e616ce27ed1
Jesse Yang <jesseyang432@gmail.com>
2021-02-02 13:57:17 -0600

    added sample data to whatido

---
+++ src/data/whatido.js
@@ -0,0 +1,16 @@
+const whatido = [
+    {
+        title: "Algorithms",
+        value: "5"
+    },
+    {
+        title: "Algebra",
+        value: "4"
+    },
+    {
+        title: "Quick Maths",
+        value: "7"
+    }
+];
+
+export default whatido;
