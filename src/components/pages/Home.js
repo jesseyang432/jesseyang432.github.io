1bc217e339e2ea68fcd8ce9be5d633e154c5357c
Jesse Yang <jesseyang432@gmail.com>
2021-02-04 20:59:48 -0600

    added Home-bio-info to center bio section

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -37,20 +37,22 @@ class Home extends Component {
                         <SocialLinks/>
                     </div>
                     <div className="Home-bio-container">
-                        <div className="Home-bio-image">
-                            <div id="Home-bio-image-hover" className="Home-bio-image-text">
-                                <p><strong>From: Chicago, IL</strong></p>
-                                <p><strong>Age: 18</strong></p>
+                        <div className="Home-bio-info">
+                            <div className="Home-bio-image">
+                                <div id="Home-bio-image-hover" className="Home-bio-image-text">
+                                    <p><strong>From: Chicago, IL</strong></p>
+                                    <p><strong>Age: 18</strong></p>
+                                </div>
+                                <img src="/images/jesseyang.jpg" alt="Jesse Yang"/>
                             </div>
-                            <img src="/images/jesseyang.jpg" alt="Jesse Yang"/>
-                        </div>
-                        <div className="Home-bio-description">
-                            <p>I'm an MIT freshman hoping to</p>
-                            <ul>
-                                <li><span className="first-word">Build</span> something people can enjoy</li>
-                                <li><span className="first-word">Learn</span> something to help make the world a better place</li>
-                                <li><span className="first-word">Write</span> something people can relate to</li>
-                            </ul>
+                            <div className="Home-bio-description">
+                                <p>I'm an MIT freshman hoping to</p>
+                                <ul>
+                                    <li><span className="first-word">Build</span> something people can enjoy</li>
+                                    <li><span className="first-word">Learn</span> something to help make the world a better place</li>
+                                    <li><span className="first-word">Write</span> something people can relate to</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                     <div className="Home-explore">
