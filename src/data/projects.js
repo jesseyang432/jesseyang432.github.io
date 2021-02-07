ed8d421b33afe90507eb958fb134ded7ea4c6bd8
Jesse Yang <jesseyang432@gmail.com>
2021-02-06 20:42:33 -0600

    added descriptions to projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,12 +2,14 @@ const projects = [
     {
         id: "converge",
         title: "Converge",
+        description: "A group organization web app featuring live private and group chatting as well as tracking of due date, priority, and completion of individual/group to-dos, payments, and events.",
         technologies: ["React", "Node.js", "Express", "MongoDB", "Socket.IO"],
         src: "images/converge.png"
     },
     {
         id: "ccc",
         title: "Chinese Culture Connection",
+        description: "A new, responsive website with accessible and meaningful frontend design for the Chinese Culture Connection, a non-profit organization in Malden, MA focused on promoting cross cultural understanding.",
         technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"],
         src: "images/ccc.png"
     }
