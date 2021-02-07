e843a779287a2bc7edbd9562f9f1473f173e30f0
Jesse Yang <jesseyang432@gmail.com>
2021-02-06 19:49:51 -0600

    added src properties with image path to projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,12 +2,14 @@ const projects = [
     {
         id: "converge",
         title: "Converge",
-        technologies: ["React", "Node.js", "Express", "MongoDB", "Socket.IO"]
+        technologies: ["React", "Node.js", "Express", "MongoDB", "Socket.IO"],
+        src: "images/converge.png"
     },
     {
         id: "ccc",
         title: "Chinese Culture Connection",
-        technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"]
+        technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+        src: "images/ccc.png"
     }
 ];
 
