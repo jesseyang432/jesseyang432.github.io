6e29a09c8f5cbb3ab0143fce60873d407f88a3fc
Jesse Yang <jesseyang432@gmail.com>
2021-02-06 19:15:54 -0600

    added slightly more info to projects data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,11 @@
 const projects = [
     {
-        title: "Converge"
+        title: "Converge",
+        technologies: ["React", "Node.js", "Express", "MongoDB", "Socket.IO"]
     },
     {
-        title: "Chinese Culture Connection"
+        title: "Chinese Culture Connection",
+        technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"]
     }
 ];
 
